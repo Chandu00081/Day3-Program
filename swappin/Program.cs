3462ca9ac9ba5102572101645a2865681126289f
chandu00081 <chandu00081@gmail.com>
2023-02-05 13:44:48 +0530

    Initial commit

---
+++ swappin/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace swappin
+{
+    internal class Swap
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("enter 1st number:");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter 2nd number");
+            int z = int.Parse(Console.ReadLine());
+            x = y;
+            y = z;
+            y = x;
+            Console.WriteLine("After swapping:");
+
+        }
+    }
+}
